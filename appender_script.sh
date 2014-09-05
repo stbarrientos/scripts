b0f0c457a6c29631cebd5acc76f5523dde6e7827
stbarrientos <stbarrientos@gmail.com>
2014-09-05 10:32:15 -0400

    back to basics

--- appender_script.sh
+++ appender_script.sh
@@ -3,10 +3,10 @@
 
 FILE=~/.bash_profile
 echo "Appending $FILE"
-"\nalias assassinate=\"ruby ~/hazardous_material/dummy/path/for/finder/to/traverse/looking/for/the/target/assassin.rb\"\n" >> $FILE
-"parse_git_branch() {\n" >> $FILE
-"\tgit rev-parse --abbrev-ref HEAD 2> /dev/null | sed 's/^/ /'\n" >> $FILE
-"}" >> $FILE
+echo -e "\nalias assassinate=\"ruby ~/hazardous_material/dummy/path/for/finder/to/traverse/looking/for/the/target/assassin.rb\"\n" >> $FILE
+echo -e "parse_git_branch() {\n" >> $FILE
+echo -e "\tgit rev-parse --abbrev-ref HEAD 2> /dev/null | sed 's/^/ /'\n" >> $FILE
+echo -e "}" >> $FILE
 CURSOR="➠"
-"export PS1=\"\h: \W\[\\033[33m\]\$(parse_git_branch)\[\\033[00m\] $CURSOR  \"\n" >> $FILE
-echo "$FILE has been successfully edited."
+echo -e "export PS1=\"\h: \W\[\\033[33m\]\$(parse_git_branch)\[\\033[00m\] $CURSOR  \"\n" >> $FILE
+echo -e "$FILE has been successfully edited."
